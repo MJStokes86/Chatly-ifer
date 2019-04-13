2f50b6765d2eaddb9cfb4fd867d1f48faf6ec00b
mjstokes86 <mjstokes1986@att.net>
2019-04-13 02:26:19 -0400

    deleted script tag in index.html which resolved the double data issue from the api

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
-/*=============================!!!!NOTE!!!!===============================*/
 
-// When I fetch the api json data, I get double the results. Using .forEach() method, I get double data printed to the HTML. The issue may have something to due with the WebPack config file. I couldn't figure it out but according to the Network tab in Chrome, I do see two fetch requests. One from VM and the other from app.js. They are both being printed to the HTML. You'll will see repeated messages once the page is loaded. 
-
-// Also the image url from the api isn't working. That's the reason why the image parameter isn't included from the data. 
-
-
-/*=================================================================*/
 
 import './sass/main.scss';
 
